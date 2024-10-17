af6ff7013b82a618136a237775ff34e0bcbcd873
Rami <ramiiya02r@gmail.com>
2024-10-17 15:34:53 +0300

    adding fluent validation .1

--- src/InvoiceSystem.Domain.Shared/InvoiceSystemDomainErrorCodes.cs
+++ src/InvoiceSystem.Domain.Shared/InvoiceSystemDomainErrorCodes.cs
@@ -2,10 +2,7 @@
 
 public static class InvoiceSystemDomainErrorCodes
 {
-    public const string PRODUCT_NOT_FOUND = "STORE:PRODUCTS:0001";
-    public const string INVALID_Invoice_DATA_NAME_AR = "STORE:PRODUCTS:0002";
-    public const string INVALID_PRODUCT_DATA_NAME_EN = "STORE:PRODUCTS:0003";
-    public const string INVALID_PRODUCT_DATA_DESC_AR = "STORE:PRODUCTS:0004";
-    public const string INVALID_PRODUCT_DATA_DESC_EN = "STORE:PRODUCTS:0005";
-    public const string INVALID_PRODUCT_CATEGORY = "STORE:PRODUCTS:0006";
+   
+    public const string INVALID_Invoice_DATA_NAME = "0002";
+    
 }
